729c2be0d42d452f0521aa4d65265b7e7d73c5a3
Gregg, Nathan <negregg@mail.wvu.edu>
2019-09-09 14:14:31 -0400

    Initial commit of programming assignment 1

---
+++ natemath.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+
+import math
+
+# Adds two values and returns a result
+def addValue(value1, value2):
+    result = value1 + value2
+    return result
+
+# Substracts two values and returns a result
+def subValue(value1, value2):
+    result = value1 - value2
+    return result
+
+# Multiplies two values and returns a result
+def multiValue(value1, value2):
+    result = value1 * value2
+    return result
+
+# Divides value 1 by value 2 and returns the result
+def divValue(value1, value2):
+    result = value1 / value2
+    return result
+
+# Finds square root of a value
+def sqrtValue(value):
+    math.sqrt(value);
+    return result
+
+# Find exponent value ( i.e. value1^(value2))
+def expValue(value1, value2):
+    result = value1 + value2
+    return result
+
+# Returns octal value as int
+def oct2Int(value):
+    result = dec(value)
+    return result
+
+# Returns hexadecimal as int
+def hex2Int(value):
+    result = dec(value)
+    return result
+
+# Returns binary as int
+def bin2Int(value):
+    result = dec(value)
+    return result
+
+# Returns hex as oct
+def hex2Oct(value):
+    result = oct(value)
+    return result
+
+# Returns int as oct
+def int2Oct(value):
+    result = oct(value)
+    return result
+
+# Returns bin as oct
+def bin2Oct(value):
+    result = oct(value)
+    return result
+
+# Returns oct as hex
+def oct2Hex(value):
+    result = hex(value)
+    return result
+
+# Returns int as hex
+def int2Hex(value):
+    result = hex(value)
+    return result
+
+# Returns bin as hex
+def bin2Hex(value):
+    result = hex(value)
+    return result
+
+# Returns oct as bin
+def oct2Bin(value):
+    result = bin(value)
+    return result
+
+# Returns int as bin
+def int2Bin(value):
+    result = bin(value)
+    return result
+
+# Returns hex as bin
+def hex2Bin(value):
+    result = bin(value)
+    return result
---
+++ tester.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import natemath
+
+print(addValue(5,10))
+ans = addValue(5,10)
+
+print (ans)
+
